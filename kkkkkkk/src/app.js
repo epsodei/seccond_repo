e0e0abc201a26958404be0e0a2a9f608a70462f1
epsodei <epsodei1@naver.com>
2014-10-31 15:11:10 +0900

    branch_first

--- kkkkkkk/src/app.js
+++ kkkkkkk/src/app.js
@@ -1,69 +1,63 @@
 
 var HelloWorldLayer = cc.Layer.extend({
     sprite:null,
+    ball:null,
     ctor:function () {
         //////////////////////////////
         // 1. super init first
         this._super();
 
-        /////////////////////////////
-        // 2. add a menu item with "X" image, which is clicked to quit the program
-        //    you may modify it.
-        // ask the window size
-        var size = cc.winSize;
-
-        // add a "close" icon to exit the progress. it's an autorelease object
-        var closeItem = new cc.MenuItemImage(
-            res.CloseNormal_png,
-            res.CloseSelected_png,
-            function () {
-                cc.log("Menu is clicked!");
-            }, this);
-        closeItem.attr({
-            x: size.width - 20,
-            y: 20,
-            anchorX: 0.5,
-            anchorY: 0.5
-        });
-
-        var menu = new cc.Menu(closeItem);
-        menu.x = 0;
-        menu.y = 0;
-        this.addChild(menu, 1);
-
-        /////////////////////////////
-        // 3. add your codes below...
-        // add a label shows "Hello World"
-        // create and initialize a label
-        var helloLabel = new cc.LabelTTF("Hello World", "Arial", 38);
-        // position the label on the center of the screen
-        helloLabel.x = size.width / 2;
-        helloLabel.y = 0;
-        // add the label as a child to this layer
-        this.addChild(helloLabel, 5);
-
-        // add "HelloWorld" splash screen"
-        this.sprite = new cc.Sprite(res.HelloWorld_png);
-        this.sprite.attr({
-            x: size.width / 2,
-            y: size.height / 2,
-            scale: 0.5,
-            rotation: 180
-        });
-        this.addChild(this.sprite, 0);
-
-        this.sprite.runAction(
-            cc.sequence(
-                cc.rotateTo(2, 0),
-                cc.scaleTo(2, 1, 1)
-            )
-        );
-        helloLabel.runAction(
-            cc.spawn(
-                cc.moveBy(2.5, cc.p(0, size.height - 40)),
-                cc.tintTo(2.5,255,125,0)
-            )
-        );
+        //ball 생성
+        this.ball = new cc.Sprite("res/ball.png")
+        //초기 위치 및 크기 설정
+        this.ball.x = this.width/2;
+        this.ball.y = this.height/2;
+        this.ball.scaleX = 0.2;
+        this.ball.scaleY = 0.18;
+        //초기 속도
+        var initialDirection = Math.random(2*Math.PI);
+        this.ball.speedX = Math.cos(initialDirection);
+        this.ball.speedY = Math.sin(initialDirection);
+        this.addChild(this.ball);
+        
+        /*this.update = function(dt){
+        	this.ball.x += this.ball.speedX;
+        	this.ball.y += this.ball.speedY;
+        	if(this.ball.x<this.ball.getBoundingBox().width/2){
+        		this.ball.speedX = -this.ball.speedX;
+        	}
+        	if(this.ball.x>this.width-this.ball.getBoundingBox().width/2){
+        		this.ball.speedX = -this.ball.speedX;
+        	}
+        	if(this.ball.y<this.ball.getBoundingBox().height/2){
+        		this.ball.speedY = -this.ball.speedY;
+        	}
+        	if(this.ball.y>this.height-this.ball.getBoundingBox().height/2){
+        		this.ball.speedY = -this.ball.speedY;
+        	}
+        }*/
+        this.update = function(dt){
+        	this.ball.x += this.ball.speedX;
+        	this.ball.y += this.ball.speedY;
+        	if(this.ball.x<this.ball.getBoundingBox().width/2){
+        		this.ball.speedX = -1;
+        		this.ball.x = 2*(this.ball.getBoundinBox().width/2)-this.ball.x;
+        	}
+        	if(this.ball.x>this.width-this.ball.getBoundingBox().width/2){
+        		this.ball.speedX = -1;
+        		this.ball.x = 2*(this.width-this.ball.getBoundingBox().width/2)-this.ball.x;
+        	}
+        	if(this.ball.y<this.ball.getBoundingBox().height/2){
+        		this.ball.speedY = -1;
+        		this.ball.y = 2*(this.ball.getBoundingBox().height/2)-this.ball.y;
+        	}
+        	if(this.ball.y>this.height-this.ball.getBoundingBox().height/2){
+        		this.ball.speedY = -1;
+        		this.ball.y = 2*(this.height-this.ball.getBoundingBox().height/2)-this.ball.y;
+        	}
+        }
+        this.scheduleUpdate();
+        
         return true;
     }
 });
